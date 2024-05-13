65ad1d0f9043d5296e147a3dc9542cccc712b1f8
David Torrubia <davidtorrubiasantos@gmail.com>
2024-05-13 12:15:34 +0200

    Restarted desired design style

---
+++ src/components/Main/LogoComponent.vue
@@ -0,0 +1,18 @@
+<template>
+  <!-- Container of the logo -->
+  <div class="flex items-center justify-center w-full">
+    <svg class="w-24 h-24">
+      <use xlink:href="../../assets/eye.svg#myIcon"></use>
+    </svg>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+export default defineComponent({
+  name: 'LogoComponent',
+  setup() {
+    return {}
+  }
+})
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,16 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+<template>
+  <div><HeaderComponent></HeaderComponent></div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+export default defineComponent({
+  name: 'HomeView',
+  components: {},
+  setup() {
+    return {}
+  }
+})
 </script>
 
-<template>
-  <main>
-    <TheWelcome />
-  </main>
-</template>
+<style scoped></style>
